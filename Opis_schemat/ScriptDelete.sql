13563affbb37fa7cecfdd8495f7d4fcf944c3a64
Piro13 <44500341+Piro13@users.noreply.github.com>
2018-11-16 15:02:22 +0100

    Update ScriptDelete.sql

---
+++ Opis_schemat/ScriptDelete.sql
@@ -0,0 +1,23 @@
+drop table CALVING;
+
+drop table MEDS_USED;
+
+drop table TREATMENT;
+
+drop table STATS_MONTHLY;
+
+drop table STATS_DAILY;
+
+drop table INSEMINATION;
+
+drop table TEAM_CATTLE;
+
+drop table CATTLE;
+
+drop table TEAM;
+
+drop table MEDICINE;
+
+drop table BULL;
+
+drop table COWSHED;
