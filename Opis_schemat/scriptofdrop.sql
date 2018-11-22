75d3601b46a6776c157615092c4ceb18756e7c3f
Mateusz Samluk <mazsak97@gmail.com>
2018-11-22 12:04:45 +0100

    Z classami

---
+++ Opis_schemat/scriptofdrop.sql
@@ -0,0 +1,288 @@
+
+    alter table CALVING 
+       drop 
+       foreign key FKlx00jg1o7crum0i3dpwbsatr5
+
+    alter table CALVING 
+       drop 
+       foreign key FKa0ab9x5ny55u9mgpf9qxanmsd
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKbpmv1fp9h3lbcfp6u0wawhamq
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKs15g8m7bwtqv6qc7t7gxqxbic
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FKpv0qqgiyfise7dea37gsg1fju
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FK3xgjc4jmv93wi73cnoewtas1w
+
+    alter table STATS_DAILY 
+       drop 
+       foreign key FKiw61d1e8xwx08fheecantyp6e
+
+    alter table STATS_MONTHLY 
+       drop 
+       foreign key FKj4yxhj9oyxtch5egnp0f60nob
+
+    alter table TEAM 
+       drop 
+       foreign key FKe6mwbelo5tkk3b5dhkk5q13ti
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKmkba7265nlm3ijmt7w5x3aw1u
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKgbtb7y6tck486rtn9flaprh7a
+
+    alter table TREATMENT 
+       drop 
+       foreign key FKqqe2nkqcctxj5ou4sv2vroft5
+
+    drop table if exists BULL
+
+    drop table if exists CALVING
+
+    drop table if exists CATTLE
+
+    drop table if exists COWSHED
+
+    drop table if exists INSEMINATION
+
+    drop table if exists MEDICINE
+
+    drop table if exists MEDS_USED
+
+    drop table if exists STATS_DAILY
+
+    drop table if exists STATS_MONTHLY
+
+    drop table if exists TEAM
+
+    drop table if exists TEAM_CATTLE
+
+    drop table if exists TREATMENT
+
+    alter table CALVING 
+       drop 
+       foreign key FKlx00jg1o7crum0i3dpwbsatr5
+
+    alter table CALVING 
+       drop 
+       foreign key FKa0ab9x5ny55u9mgpf9qxanmsd
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKbpmv1fp9h3lbcfp6u0wawhamq
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKs15g8m7bwtqv6qc7t7gxqxbic
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FKpv0qqgiyfise7dea37gsg1fju
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FK3xgjc4jmv93wi73cnoewtas1w
+
+    alter table STATS_DAILY 
+       drop 
+       foreign key FKiw61d1e8xwx08fheecantyp6e
+
+    alter table STATS_MONTHLY 
+       drop 
+       foreign key FKj4yxhj9oyxtch5egnp0f60nob
+
+    alter table TEAM 
+       drop 
+       foreign key FKe6mwbelo5tkk3b5dhkk5q13ti
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKmkba7265nlm3ijmt7w5x3aw1u
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKgbtb7y6tck486rtn9flaprh7a
+
+    alter table TREATMENT 
+       drop 
+       foreign key FKqqe2nkqcctxj5ou4sv2vroft5
+
+    drop table if exists BULL
+
+    drop table if exists CALVING
+
+    drop table if exists CATTLE
+
+    drop table if exists COWSHED
+
+    drop table if exists INSEMINATION
+
+    drop table if exists MEDICINE
+
+    drop table if exists MEDS_USED
+
+    drop table if exists STATS_DAILY
+
+    drop table if exists STATS_MONTHLY
+
+    drop table if exists TEAM
+
+    drop table if exists TEAM_CATTLE
+
+    drop table if exists TREATMENT
+
+    alter table CALVING 
+       drop 
+       foreign key FKlx00jg1o7crum0i3dpwbsatr5
+
+    alter table CALVING 
+       drop 
+       foreign key FKa0ab9x5ny55u9mgpf9qxanmsd
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKbpmv1fp9h3lbcfp6u0wawhamq
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKs15g8m7bwtqv6qc7t7gxqxbic
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FKpv0qqgiyfise7dea37gsg1fju
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FK3xgjc4jmv93wi73cnoewtas1w
+
+    alter table STATS_DAILY 
+       drop 
+       foreign key FKiw61d1e8xwx08fheecantyp6e
+
+    alter table STATS_MONTHLY 
+       drop 
+       foreign key FKj4yxhj9oyxtch5egnp0f60nob
+
+    alter table TEAM 
+       drop 
+       foreign key FKe6mwbelo5tkk3b5dhkk5q13ti
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKmkba7265nlm3ijmt7w5x3aw1u
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKgbtb7y6tck486rtn9flaprh7a
+
+    alter table TREATMENT 
+       drop 
+       foreign key FKqqe2nkqcctxj5ou4sv2vroft5
+
+    drop table if exists BULL
+
+    drop table if exists CALVING
+
+    drop table if exists CATTLE
+
+    drop table if exists COWSHED
+
+    drop table if exists INSEMINATION
+
+    drop table if exists MEDICINE
+
+    drop table if exists MEDS_USED
+
+    drop table if exists STATS_DAILY
+
+    drop table if exists STATS_MONTHLY
+
+    drop table if exists TEAM
+
+    drop table if exists TEAM_CATTLE
+
+    drop table if exists TREATMENT
+
+    alter table CALVING 
+       drop 
+       foreign key FKlx00jg1o7crum0i3dpwbsatr5
+
+    alter table CALVING 
+       drop 
+       foreign key FKa0ab9x5ny55u9mgpf9qxanmsd
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKbpmv1fp9h3lbcfp6u0wawhamq
+
+    alter table INSEMINATION 
+       drop 
+       foreign key FKs15g8m7bwtqv6qc7t7gxqxbic
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FK3xgjc4jmv93wi73cnoewtas1w
+
+    alter table MEDS_USED 
+       drop 
+       foreign key FKpv0qqgiyfise7dea37gsg1fju
+
+    alter table STATS_DAILY 
+       drop 
+       foreign key FKiw61d1e8xwx08fheecantyp6e
+
+    alter table STATS_MONTHLY 
+       drop 
+       foreign key FKj4yxhj9oyxtch5egnp0f60nob
+
+    alter table TEAM 
+       drop 
+       foreign key FKe6mwbelo5tkk3b5dhkk5q13ti
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKmkba7265nlm3ijmt7w5x3aw1u
+
+    alter table TEAM_CATTLE 
+       drop 
+       foreign key FKgbtb7y6tck486rtn9flaprh7a
+
+    alter table TREATMENT 
+       drop 
+       foreign key FKqqe2nkqcctxj5ou4sv2vroft5
+
+    drop table if exists BULL
+
+    drop table if exists CALVING
+
+    drop table if exists CATTLE
+
+    drop table if exists COWSHED
+
+    drop table if exists INSEMINATION
+
+    drop table if exists MEDICINE
+
+    drop table if exists MEDS_USED
+
+    drop table if exists STATS_DAILY
+
+    drop table if exists STATS_MONTHLY
+
+    drop table if exists TEAM
+
+    drop table if exists TEAM_CATTLE
+
+    drop table if exists TREATMENT
