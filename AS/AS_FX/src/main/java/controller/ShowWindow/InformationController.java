8d8980d1a39e8867773b565730689a0ebbc5493b
Mateusz Samluk <mazsak97@gmail.com>
2018-12-05 23:32:12 +0100

    Wiecej GUI

---
+++ AS/AS_FX/src/main/java/controller/ShowWindow/InformationController.java
@@ -0,0 +1,40 @@
+package controller.ShowWindow;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InformationController implements Initializable {
+
+    @FXML
+    private Label birthDateCattle;
+
+    @FXML
+    private Label nameCattle;
+
+    @FXML
+    private Label joinDateCattle;
+
+    @FXML
+    private Label leaveReasonCattle;
+
+    @FXML
+    private Label earringCattle;
+
+    @FXML
+    private Label cowshedNumberCattle;
+
+    @FXML
+    private Label sexCattle;
+
+    @FXML
+    private Label leaveDateCattle;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
