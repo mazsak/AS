5dfce056198d5524edd45e81ce2b6d92fa4f0785
Andzik <smers20@wp.pl>
2019-01-04 19:03:20 +0100

    checkboxy w dodawaniu krowy

--- AS/AS_FX/src/main/java/controller/AddWindow/AddCattleController.java
+++ AS/AS_FX/src/main/java/controller/AddWindow/AddCattleController.java
@@ -1,17 +1,28 @@
 package controller.AddWindow;
 
+import hibernate.FactoryHibernate;
+import hibernate.HCowshed;
+import hibernate.HTeam;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import models.Cowshed;
+import models.Team;
 
+import javax.persistence.EntityManager;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AddCattleController implements Initializable {
+    private EntityManager em;
 
     @FXML
     private TextField earring;
@@ -26,7 +37,7 @@ public class AddCattleController implements Initializable {
     private TextField leaveReason;
 
     @FXML
-    private ComboBox<?> sex;
+    private ComboBox<String> sex;
 
     @FXML
     private TextField name;
@@ -38,13 +49,13 @@ public class AddCattleController implements Initializable {
     private DatePicker leaveDate;
 
     @FXML
-    private ComboBox<?> cowshed;
+    private ComboBox<String> cowshed;
 
     @FXML
     private DatePicker birthDate;
 
     @FXML
-    private ComboBox<?> group;
+    private ComboBox<String> group;
 
     @FXML
     void addCattleActionListener(ActionEvent event) {
@@ -53,7 +64,28 @@ public class AddCattleController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        em = FactoryHibernate.getEm();
+        sex.getItems().addAll("XX", "XY");
 
+        List<Cowshed> csh = HCowshed.read(em);
+
+        ObservableList<String> cowsheds = FXCollections.observableArrayList();
+
+        for (int i = 0; i < csh.size(); i++) {
+            cowsheds.add(csh.get(i).getName());
+        }
+
+        cowshed.setItems(cowsheds);
+    }
+
+    @FXML
+    public void checkedActionListener(ActionEvent event) {
+        List<Team> tm = HTeam.getByCowshedName(em, cowshed.getSelectionModel().getSelectedItem());
+        ObservableList<String> groups = FXCollections.observableArrayList();
+        for (int i = 0; i < tm.size(); i++) {
+            groups.add(tm.get(i).getName());
+        }
+        group.setItems(groups);
     }
 }
 
