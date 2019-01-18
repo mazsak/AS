8ec4f88d567f5074492fb903e30e8ee3d52db31e
Mateusz Samluk <mazsak97@gmail.com>
2019-01-18 22:19:31 +0100

    cos z editgroup

--- AS/AS_FX/src/main/java/controller/EditWindow/EditGroupController.java
+++ AS/AS_FX/src/main/java/controller/EditWindow/EditGroupController.java
@@ -1,10 +1,15 @@
 package controller.EditWindow;
 
+import hibernate.HCowshed;
+import hibernate.HTeam;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import models.Cowshed;
 import models.Team;
 
 import java.net.URL;
@@ -14,7 +19,8 @@ import java.util.ResourceBundle;
 
 public class EditGroupController implements Initializable {
 
-    private List<Team> groups;
+    private List<Team> groupsFirstList;
+    private List<Team> groupsSecondList;
 
     @FXML
     private ComboBox<String> groupSecond;
@@ -23,10 +29,10 @@ public class EditGroupController implements Initializable {
     private ComboBox<String> groupFirst;
 
     @FXML
-    private ComboBox<?> cowshedFirst;
+    private ComboBox<String> cowshedFirst;
 
     @FXML
-    private ComboBox<?> cowshedSecond;
+    private ComboBox<String> cowshedSecond;
 
     @FXML
     private ComboBox<String> type;
@@ -39,11 +45,32 @@ public class EditGroupController implements Initializable {
 
     @FXML
     void cowshedFirstActionListener(ActionEvent event) {
-
+        groupsFirstList = HTeam.getByCowshedName(cowshedFirst.getSelectionModel().getSelectedItem());
+        ObservableList<String> groups2 = FXCollections.observableArrayList();
+        for (int i = 0; i < groupsFirstList.size(); i++) {
+            groups2.add(groupsFirstList.get(i).getName());
+        }
+        groupFirst.setItems(groups2);
+        if (cowshedSecond.getSelectionModel().isEmpty()) {
+            cowshedSecond.getSelectionModel().select(cowshedFirst.getSelectionModel().getSelectedIndex());
+            groupsSecondList.addAll(groupsFirstList);
+            groupSecond.setItems(groups2);
+        }
     }
 
     @FXML
     void cowshedSecondActionListener(ActionEvent event) {
+        groupsSecondList = HTeam.getByCowshedName(cowshedSecond.getSelectionModel().getSelectedItem());
+        ObservableList<String> groups2 = FXCollections.observableArrayList();
+        for (int i = 0; i < groupsSecondList.size(); i++) {
+            groups2.add(groupsSecondList.get(i).getName());
+        }
+        groupSecond.setItems(groups2);
+        if (groupFirst.getSelectionModel().isEmpty()) {
+            cowshedFirst.getSelectionModel().select(cowshedSecond.getSelectionModel().getSelectedIndex());
+            groupsFirstList.addAll(groupsSecondList);
+            groupFirst.setItems(groups2);
+        }
 
     }
 
@@ -65,7 +92,6 @@ public class EditGroupController implements Initializable {
     @FXML
     void typeActionListener(ActionEvent event) {
         if (type.getSelectionModel().getSelectedItem().equals("Leczone")) {
-            nameMedicine.getItems().addAll("Żywieniowe", "Leczone");
             nameMedicine.setDisable(false);
             dateStartTreatment.setDisable(false);
         } else {
@@ -76,9 +102,27 @@ public class EditGroupController implements Initializable {
         }
     }
 
+    @FXML
+    void groupFirstActionListener(ActionEvent event) {
+    }
+
+    @FXML
+    void groupSecondActionListener(ActionEvent event) {
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         dateStartTreatment.setValue(LocalDate.now());
         type.getItems().addAll("Żywieniowe", "Leczone");
+        type.getSelectionModel().select(0);
+
+        List<Cowshed> csh = HCowshed.read();
+        ObservableList<String> cowsheds = FXCollections.observableArrayList();
+        for (int i = 0; i < csh.size(); i++) {
+            cowsheds.add(csh.get(i).getName());
+        }
+
+        cowshedFirst.setItems(cowsheds);
+        cowshedSecond.setItems(cowsheds);
     }
 }
